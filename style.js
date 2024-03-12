d5340545befc60f061f8fe4f6e6cc9d7fc20a577
sadab-devloper <158466472+sadab-devloper@users.noreply.github.com>
2024-03-12 13:22:50 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,47 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+var elemC = document.querySelector("#elem-container")
+var fixed = document.querySelector("#fixed-image")
+elemC.addEventListener("mouseenter",function(){
+    fixed.style.display = "block"
+})
+elemC.addEventListener("mouseleave",function(){
+    fixed.style.display = "none"
+})
+var elem = document.querySelectorAll("#elme")
+elem.forEach(function(e){
+    e.addEventListener("mouseenter",function(){
+        var image = e.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${image})`
+    })
+})
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  var menu = document.querySelector("nav h6")
+  var full =document.querySelector("#full-scr")
+  var navimg = document.querySelector("nav img")
+  var flag = 0
+  menu.addEventListener("click", function () {
+    if(flag == 0){
+        full.style.top = 0
+        navimg.style.opacity = 0
+        flag = 1
+    }else{
+            full.style.top = "-100%"
+            navimg.style.opacity = 1
+            flag = 1
+        }
+  })
+  var loader = document.querySelector(".loader")
+  setTimeout(function(){
+    loader.style.top = "-100%"
+  },4200)
